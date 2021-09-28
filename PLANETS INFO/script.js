47c73c28a1bfef71ae1745ee4ab6d09a7f3c40c4
bandeira <thiagooce@gmail.com>
2021-09-27 23:10:35 -0300

    files commit

--- PLANETS INFO/script.js
+++ PLANETS INFO/script.js
@@ -1,6 +1,9 @@
-fetch('planets-data.json')
+//fetch('planets-data.json')
 function planetImg(x) {
   let chosenPlanet = x
   let planetImg = document.querySelector('.planet-main-img')
-  planetImg.setAttribute('src', `./img/${planetName}300px.png`)
+  planetImg.setAttribute('src', `./img/${chosenPlanet}300px.png`)
+
+  let planetBioTitle = document.querySelector('.planet-bio-title')
+  planetBioTitle.innerHTML = chosenPlanet.toUpperCase()
 }
